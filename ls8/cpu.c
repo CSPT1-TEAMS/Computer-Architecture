b2386d479320ebc806ddb485f3fb31f06d288118
Alexander <alexander.botello@g.austincc.edu>
2018-11-02 19:37:47 -0500

    write cpu_init and cpu_run

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -57,11 +57,98 @@ void cpu_run(struct cpu *cpu)
   int running = 1; // True until we get a HLT instruction
 
   while (running) {
-    // TODO
-    // 1. Get the value of the current instruction (in address PC).
-    // 2. switch() over it to decide on a course of action.
-    // 3. Do whatever the instruction should do according to the spec.
-    // 4. Move the PC to the next instruction.
+    unsigned char IR = cpu_ram_read(cpu. cpu->pc);
+    unsigned char operandA = cpu_ram_read(cpu, (cpu->pc +1)& MAX_ADDR);
+    unsigned char operandB = cpu_ram_read(cpu, (cpu->pc + 2)& MAX_ADDR);
+
+    switch(IR) {
+      case PRN:
+        printf("%d\n", cpu->reg[operandA]);
+        break;
+
+      case LDI:   // LDI/load instruction
+        cpu->reg[operandA] = operandB;
+        break;
+
+      case HLT: // halt method
+        running = 0;
+        break;
+
+      case MUL:
+        alu(cpu, ALU_MUL, operandA, operandB);
+        break;
+
+      case ADD:
+        alu(cpu, ALU_ADD, operandA, operandB);
+        break;
+
+      case PUSH:  // add push function
+        cpu_push(cpu, cpu->reg[operandA]);
+        break;
+
+      case POP:
+        cpu->reg[operandA] = cpu_pop(cpu);
+        break;
+
+      case PRA:
+        printf("%c\n", cpu->reg[operandA]);
+        break;
+
+      case CALL:  // Call;
+        cpu_push(cpu, cpu->pc + 2);
+        cpu->pc = cpu->reg[operandA];
+        break;
+
+      case RET:
+        cpu->pc = cpu_pop(cpu);
+        break;
+
+      case CMP:
+        if (cpu->reg[operandA] == cpu->reg[operandB])
+        {
+          cpu->fl = 1;
+        } else
+        {
+          cpu->fl = 0;
+        }
+        break;
+
+      case JMP:
+        cpu->pc = cpu->reg[operandA];
+        break;
+
+      case JEQ:
+        if (cpu->fl)
+        {
+          cpu->pc = cpu->reg[operandA];
+        }
+        else
+        {
+          cpu->pc += 2;
+        }
+        break;
+
+      case JNE:
+        if (!cpu->fl)
+        {
+          cpu->pc = cpu->reg[operandA];
+        }
+        else
+        {
+          cpu->pc += 2;
+        }
+        break;
+
+      default:
+        printf("Unknown instruction at %02x: %02x\n", cpu->pc, IR);
+        exit(2);
+    }
+    int instruction_set_pc = (IR >> 4) & 1;
+
+    if (!instruction_set_pc)
+    {
+      cpu->pc += ((IR >> 6) & 0x3) + 1;
+    }
   }
 }
 
@@ -70,7 +157,14 @@ void cpu_run(struct cpu *cpu)
  */
 void cpu_init(struct cpu *cpu)
 {
-  // TODO: Initialize the PC and other special registers
+  cpu->pc = START_OF_PROGRAM_ADDR;
+  cpu->flag CPU_FLAG;
+
+  cpu->reg[SP] = START_OF_STACK_ADDR;
+  cpu->reg[IM] = INTERRUPT_MASK;
+  cpu->reg[IS] = INTERRUPTS;
 
   // TODO: Zero registers and RAM
+  memset(cpu->reg, 0, sizeof cpu-reg);
+  memset(cpu->ram, 0, sizeof cpu->ram);
 }
