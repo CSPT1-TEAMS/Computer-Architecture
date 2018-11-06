863dc498103889b92fe144657175a07ae42723cf
Charlie <ccariste@gmail.com>
2018-11-05 17:53:32 -0800

    CRC: added cpu and ls8 code from class

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "cpu.h"
 
 #define DATA_LEN 6
@@ -5,40 +8,57 @@
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(char *filename, struct cpu *cpu)
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
+  FILE *fp;
+  char line[1024];
+  int address = ADDR_PROGRAM_ENTRY;
 
-  int address = 0;
-
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
+  // Open the source file
+  if ((fp = fopen(filename, "r")) == NULL) {
+    fprintf(stderr, "Cannot open file %s\n", filename);
+    exit(2);
   }
 
-  // TODO: Replace this with something less hard-coded
+  // Read all the lines and store them in RAM
+  while (fgets(line, sizeof line, fp) != NULL) {
+
+    // Convert string to a number
+    char *endchar;
+    unsigned char byte = strtol(line, &endchar, 2);;
+
+    // Ignore lines from whicn no numbers were read
+    if (endchar == line) {
+      continue;
+    }
+
+    // Store in ram
+    cpu->ram[address++] = byte;
+  }
 }
+
 
 /**
  * ALU
  */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
+  unsigned char *reg = cpu->reg;
+
+  unsigned char valB = reg[regB];
+
   switch (op) {
     case ALU_MUL:
-      // TODO
+      reg[regA] *= valB;
       break;
 
-    // TODO: implement more ALU ops
+    case ALU_ADD:
+      reg[regA] += valB;
+      break;
   }
+
 }
+
 
 /**
  * Run the CPU
@@ -66,20 +86,77 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 // the instruction opcode. See the LS-8 spec for details.
 void cpu_run(struct cpu *cpu)
 {
+  // Just so we don't have to type cpu-> every time
+  unsigned char *reg = cpu->reg;
+  unsigned char *ram = cpu->ram;
+
   int running = 1; // True until we get a HLT instruction
 
   while (running) {
-    // TODO
-    // 1. Get the value of the current instruction (in address PC).
-    // 2. switch() over it to decide on a course of action.
-    // 3. Do whatever the instruction should do according to the spec.
-    // 4. Move the PC to the next instruction.
+    unsigned char IR = ram[cpu->PC];
 
-    unsigned char IR;
-    unsigned char operandA = cpu_ram_read(cpu, cpu->PC + 1);
-    unsigned char operandB = cpu_ram_read(cpu, cpu->PC + 2);
+    unsigned char operandA = ram[(cpu->PC + 1) & 0xff];
+    unsigned char operandB = ram[(cpu->PC + 2) & 0xff];
+
+    // True if this instruction might set the PC
+    int instruction_set_pc = (IR >> 4) & 1;
+
+    // switch (IR) {
+
+    //   case LDI:
+    //     reg[operandA] = operandB;
+    //     break;
+
+    //   case PRN:
+    //     printf("%d\n", reg[operandA]);
+    //     break;
+
+    //   case MUL:
+    //     alu(cpu, ALU_MUL, operandA, operandB);
+    //     break;
+
+    //   case ADD:
+    //     alu(cpu, ALU_ADD, operandA, operandB);
+    //     break;
+
+    //   case HLT:
+    //     running = 0;
+    //     break;
+
+    //   case PRA:
+    //     printf("%c\n", reg[operandA]);
+    //     //printf("%c", reg[operandA]); fflush(stdout); // Without newline
+    //     break;
+
+    //   case CALL:
+    //     cpu_push(cpu, cpu->PC + 2);
+    //     cpu->PC = reg[operandA];
+    //     break;
+
+    //   case RET:
+    //     cpu->PC = cpu_pop(cpu);
+    //     break;
+
+    //   case PUSH:
+    //     cpu_push(cpu, reg[operandA]);
+    //     break;
+
+    //   case POP:
+    //     reg[operandA] = cpu_pop(cpu);
+    //     break;
+
+    //   default:
+    //     fprintf(stderr, "PC %02x: unknown instruction %02x\n", cpu->PC, IR);
+    //     exit(3);
+    // }
+
+    if (!instruction_set_pc) {
+      cpu->PC += ((IR >> 6) & 0x3) + 1;
+    }
+
   }
 }
+
 
 /**
  * Initialize a CPU struct
@@ -99,7 +176,7 @@ void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
   cpu->PC = 0;
-  memset(cpu->registers, 0, sizeof(int)*8);
+  memset(cpu->reg, 0, sizeof(int)*8);
   memset(cpu->ram, 0, sizeof(int)*256);
   // TODO: Zero registers and RAM
 
